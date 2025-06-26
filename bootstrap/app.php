722eaf6c036da272868ed7496909cb87f3af8d4e
Abdul Wasay <a.wasay1612@gmail.com>
2025-06-26 18:24:28 +0500

    remove public token

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-            'public_apis' => \App\Http\Middleware\ValidateApiToken::class,
         ]);
 
         //
--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,9 @@ Route::get('/get-token', function () {
     return \App\Models\ApiToken::where('valid_for', now()->toDateString())->first();
 });
 
-Route::middleware('public_apis')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register-client', [AuthController::class, 'registerClient']);
-    Route::post('/google', [AuthController::class, 'GoogleTest']);
-});
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register-client', [AuthController::class, 'registerClient']);
+Route::post('/google', [AuthController::class, 'GoogleTest']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
